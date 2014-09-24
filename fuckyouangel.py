e3e066b8a6e6e51c4b8eccb3f3871696c50af874
notia81 <fuxin81@gmail.com>
2014-09-24 17:17:38 -0400

    updated angel crawler....still buggy

--- fuckyouangel.py
+++ fuckyouangel.py
@@ -4,7 +4,8 @@ import socket
 import urllib
 import datetime
 import re
-
+import time
+import random
 import socks  # SockiPy module
 import stem.process
 from stem.util import term
@@ -22,24 +23,26 @@ API_URL = "https://api.angel.co/1/users/%i"
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
 socket.socket = socks.socksocket
 
-def open_tor ():						#sets up a new connection and tests the ip address
-	exitcode = exits[random.randint(0,5)]
-  	tor_process = stem.process.launch_tor_with_config(
-    config = {
-      'SocksPort': str(SOCKS_PORT),
-      'ExitNodes': "{%s}" % exitcode ,
-    },
-    )
-    ip = query("https://www.atagar.com/echo.php")
-	ip = re.search('\d+\.\d+\.\d+\.\d+',ip)
-	ip = ip.group(0)
-	return ip
-
 def query(url):
   try:				# Uses urllib to fetch a site using SocksiPy for Tor over the SOCKS_PORT.
     return urllib.urlopen(url).read()
   except:
     return "Unable to reach %s" % url
+
+def open_tor():						#sets up a new connection and tests the ip address
+	exitcode = exits[random.randint(0,5)]
+  	tor_process = stem.process.launch_tor_with_config(
+	    config = {
+	      'SocksPort': str(SOCKS_PORT),
+	      'ExitNodes': "{%s}" % exitcode ,
+	    }
+    )
+	ip = query("https://www.atagar.com/echo.php")
+	ip = re.search('\d+\.\d+\.\d+\.\d+',ip)
+	ip = ip.group(0)
+	return ip
+
+
 
 def updatestack(): #removes ip addresses that are no longer blacklisted
 	currenttime = time.time()
@@ -57,14 +60,22 @@ def newconnection():
 		ip = open_tor() #make sure we're not using the same ips within a single hour
 	ipstack.append(ip)
 	timestack.append(time.time())
+	print "switched up to %s\n" %ip
 
 def fuckemup():
-	for i in range(1,742618):
-		if i ==1:
+	for i in range(1,20):
+		if i == 1:
 			newconnection() #start a whole new thing
-		if i % 900 == 0:
-			tor_process.kill() #kill last one
+		if i % 5 == 0:	#reached the threshold where ban will be placed	
+			tor_process.kill()
 			newconnection()
-		pingurl = 
-
+		pingurl = API_URL % i
+		response = query(pingurl)
+		match = re.search('investor\":true',response)
+		if match: #if indeed an investor
+			f = open("%i.txt", 'w')
+			f.write(response)
+			f.close()
+			print "finished id#%i\n"%i
+fuckemup()
 
